571fd16e9abfe4a62c5e076e53a5f232121a0753
Nathan Cosgrove <ncosgrove272@gmail.com>
2023-09-16 23:17:28 -0500

    added 2 simple functions and an event

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
-const name = "Nathan" ;
+const name = "Nathan is very loud" ;
+
+console.log("Below is a simple function that is envoked")
+
+function repeat() {
+console.log("Repeat after me#1"); 
+console.log("Repeat after me#2"); 
+console.log("Repeat after me#3"); 
+}
+
+repeat() ;
+
+console.log("Below is a function that has set parameters in and out");
+
+function adder(num1, num2) {
+console.log(num1 + num2)
+}
+
+adder(60, 40);
 
 
-function changecolor = 
+function changetext() {
+    const button = document.getElementById("btn").innerHTML = "See how the text changed!";
+    }
